911433ad46ed003ccbc65a3cb2082bedcb261ad9
Ennio Meier <enniomeier@hotmail.de>
2018-03-02 15:55:31 +0100

    Added more opcodes

--- src/bazo-vm/opCodes.go
+++ src/bazo-vm/opCodes.go
@@ -1,39 +1,46 @@
 package bazo_vm
 
 const (
-	// Constants
-	NOP   byte = iota // 0x00
-	PUSH  byte = iota // 0x01
-	POP   byte = iota // 0x02
-	LOAD  byte = iota // 0x03
-	STORE byte = iota // 0x04
+	// Push Operations
+	PUSH0 byte = iota // 0x0.
+	PUSH1 byte = iota // 0x0.
+	PUSH2 byte = iota // 0x0.
+	PUSH3 byte = iota // 0x0.
 
 	// Arithmetic
-	ADD byte = iota // 0x05
-	SUB byte = iota // 0x06
-	MUL byte = iota // 0x07
-	DIV byte = iota // 0x08
-	MOD byte = iota // 0x09
-	AND byte = iota // 0x0a
-	EQ  byte = iota // 0x0b
-	NEQ byte = iota // 0x0c
-	LT  byte = iota // 0x0d
-	GT  byte = iota // 0x0e
-	LTE byte = iota // 0x0f
-	GTE byte = iota // 0x10
+	ADD byte = iota // 0x0.
+	SUB byte = iota // 0x0.
+	MUL byte = iota // 0x0.
+	DIV byte = iota // 0x0.
+	MOD byte = iota // 0x0.
+	AND byte = iota // 0x0.
+	OR  byte = iota // 0x0.
+	EQ  byte = iota // 0x0.
+	NEQ byte = iota // 0x0.
+	LT  byte = iota // 0x0.
+	GT  byte = iota // 0x0.
+	LTE byte = iota // 0x0.
+	GTE byte = iota // 0x0.
 
 	// Flow control
-	//TODO
+	NOP  byte = iota // 0x0.
+	JMP  byte = iota // 0x0.
+	JMPT byte = iota // 0x0.
+	JMPF byte = iota // 0x0.
+	CALL byte = iota // 0x0.
+	RET  byte = iota // 0x0.
 
 	// Stack
-	//TODO
+	POP   byte = iota // 0x0.
+	LOAD  byte = iota // 0x0.
+	STORE byte = iota // 0x0.
 
 	// Crypto
-	//TODO
+	SHA3 byte = iota // 0x0.
 
 	// Array
 	//TODO
 
 	// Exception
-	//TODO
+	THROW byte = iota // 0x0.
 )
